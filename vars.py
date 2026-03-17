ced94357073a3a654e93d72e6db8773297ff325e
lbsnaavideos3-create <lbsnaa.videos3@gmail.com>
2026-03-17 23:26:57 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,6 +3,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "2447366")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "5dghgfccc") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "7231089830:A") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "20527556")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "85d84f730c4e2f13bdf3efbce7d3352e") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "8513506239:AAEEzYrIt0tYvyHDIXLTxDtCOrwDLoQucsg") #Replace with your bot token
